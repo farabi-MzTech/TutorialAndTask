3feb71d2ebb22271c9a2de4dd7c474810c2a933d
farabi2 <farabi@mztech.us>
2021-03-12 10:07:36 +0600

    var string mathod

--- app.js
+++ app.js
@@ -1,2 +1,29 @@
-console.log(15);
-console.log(10);
+// console.log(15);
+// console.log(10);
+
+// let newNumber=10;
+// let oldNumber=20;
+// totalNumber = newNumber + oldNumber;
+// console.log(totalNumber);
+
+// const newNumber=100;
+// const oldNumber=212;
+
+// let isNewNumber=true;
+// console.log(isNewNumber); 
+
+// let score = 5;
+// score + 4;
+// console.log(score);
+
+// let myName ="Farabi";
+// let wish ="Good Luck";
+// console.log(myName);
+// console.log(wish);
+// console.log(`${myName} ${wish.length}`);
+// console.log(`${myName} ${myName.length}`);
+
+let myName = "   HelloW Farabi!  ";
+let method = myName .trim() .toUpperCase();
+console.log(method);
+
