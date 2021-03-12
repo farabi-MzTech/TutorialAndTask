f2c56e7a1ca701add3292719aa99f0bd9a11f796
farabi2 <farabi@mztech.us>
2021-03-12 11:37:38 +0600

    string

--- app.js
+++ app.js
@@ -24,6 +24,47 @@
 // console.log(`${myName} ${myName.length}`);
 
 // let myName = "   HelloW Farabi!  ";
-// let method = myName .trim() .toUpperCase();
+// let method = myName.trim() .toUpperCase();
 // console.log(method);
 
+
+// Math.random()
+// not working
+// let die1 = Math.floor(Math.random() * 6) +11;
+// let roll= `you rolled a ${die1} and ${die1}. the sum to ${die+die1}`
+// console.log(roll);
+
+// 20 <= 10
+
+// 10>=20
+// false
+// 20>=10
+// true
+
+// console.warn("thats warning")
+
+// console.log(" CONDITION");
+//     let random=Math.random();
+//     if(random<=.2){}
+
+
+// const num = 10;
+ 
+// if(num <= 10) {
+//     if(num >= 5) {
+//         console.log("HEY!");
+//     }
+// } else {
+//     if (num < 10) {
+//         if(num % 2 === 0){
+//             console.log("YOU GOT ME!");
+//         }
+//     }
+// }
+
+
+// let myNumber = [1, 2,3,4,5,6]
+// console.log(myNumber);
+
+// let Myname="farabi";
+// Myname[1];
